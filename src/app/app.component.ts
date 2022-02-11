177e20248003d702e40479586cd2036992abf2f7
ROLANDO DAVID SANTELI CARCAMO <rsanteli@emeal.nttdata.com>
2022-02-11 10:43:01 +0100

    Commit inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'proyectoAngular1';
+  title = 'Contador';
+  num:number = 0;
+
+  constructor(){}
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PrincipalComponent } from './principal/principal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PrincipalComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [PrincipalComponent, AppComponent]
 })
 export class AppModule { }
